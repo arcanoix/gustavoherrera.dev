ee178cc5bf82489528afc924ae3e59715ebe5a66
Gustavo Herrera <gustavoh.2312@gmail.com>
2025-03-13 13:43:56 -0400

    fix bug mode theme button

--- src/components/ModeToggle.tsx
+++ src/components/ModeToggle.tsx
@@ -4,25 +4,33 @@ import { Moon, Sun } from "lucide-react"
 import { Button } from "@/components/ui/button"
 
 export function ModeToggle() {
-  const [theme, setThemeState] = useState<"theme-light" | "dark" | "system">(
-    "theme-light"
+  const [theme, setThemeState] = useState<"light" | "dark" | "system">(
+    "light"
   )
 
   const toggleTheme = () => {
-    const newTheme = theme === "dark" ? "theme-light" : "dark"
-    setThemeState(newTheme)
-    localStorage.setItem("theme", newTheme)
+    const newTheme = theme === "dark" ? "light" : "dark";
+    setThemeState(newTheme);
+    localStorage.setItem("theme", newTheme);
+    localStorage.setItem("theme-time", Date.now().toString());
   }
 
   useEffect(() => {
     const isDarkMode = document.documentElement.classList.contains("dark")
-    setThemeState(isDarkMode ? "dark" : "theme-light")
+    setThemeState(isDarkMode ? "dark" : "light")
   }, [])
 
   useEffect(() => {
-    const isDark =
-      theme === "dark" ||
-      window.matchMedia("(prefers-color-scheme: dark)").matches
+    const isDark = theme === "dark" 
+
+    if(theme === "dark")
+    {
+      document.documentElement.classList.add("dark")
+    }
+    else
+    {
+      document.documentElement.classList.remove("dark")
+    }
 
     document.documentElement.classList[isDark ? "add" : "remove"]("dark")
   }, [theme])
@@ -34,7 +42,7 @@ export function ModeToggle() {
       className="rounded-full"
       onClick={toggleTheme}
     >
-      {theme === "theme-light" ? (
+      {theme === "light" ? (
         <Moon className="h-[1.2rem] w-[1.2rem] rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0" />
       ) : (
         <Sun className="h-[1.2rem] w-[1.2rem] rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100" />
